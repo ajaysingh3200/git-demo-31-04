e67313109b0c38b539b691152aa33982ebf3bfe3
ajaysingh3200 <ajaysingh3200@gmail.com>
2021-05-04 14:38:28 +0000

    added csv on sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-
+import csv
 print "My name Ajay Singh I am learnig git and having 10 years of exp which is a part of distributed version control "
 
 print "My name Ajay Singh i have 10 years of exp"
